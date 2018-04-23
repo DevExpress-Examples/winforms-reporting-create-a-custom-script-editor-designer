035b8704f3ac189f156c4e4264f862e39aafb59a
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ Readme.md
@@ -0,0 +1,8 @@
+# WinForms End-User Designer - How to create a custom script editor
+
+
+This example demonstrates how  to create a custom script editor in the End-User Designer.
+
+<br/>
+
+
---
+++ VB/ScriptEditorExample/Form1.vb
@@ -0,0 +1,26 @@
+﻿Imports System
+Imports System.Collections.Generic
+Imports System.ComponentModel
+Imports System.Data
+Imports System.Drawing
+Imports System.Linq
+Imports System.Text
+Imports System.Threading.Tasks
+Imports System.Windows.Forms
+Imports DevExpress.XtraReports.UI
+
+Namespace ScriptEditorExample
+    Partial Public Class Form1
+        Inherits Form
+
+        Public Sub New()
+            InitializeComponent()
+        End Sub
+
+        Private Sub button1_Click(ByVal sender As Object, ByVal e As EventArgs) Handles button1.Click
+            Dim tool As New ReportDesignTool(New XtraReport())
+            tool.DesignForm.DesignMdiController.AddService(GetType(DevExpress.XtraReports.Design.IScriptEditorService), New ScriptEditorService())
+            tool.ShowDesignerDialog()
+        End Sub
+    End Class
+End Namespace
